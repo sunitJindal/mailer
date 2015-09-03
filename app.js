f8a851927b5a8bb54232676e0a15a9571e38ba1d
sunit jindal <sunitjindal@gmail.com>
2015-09-03 17:58:41 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var path           = require('path')
+    , templatesDir   = path.join(__dirname, 'templates')
+    , emailTemplates = require('email-templates')
+    , fs = require("fs");
+
+emailTemplates(templatesDir, function(err, template) {
+
+  // Render a single email with one template
+  template('resetPin', {token:"768483"}, function(err, html, text) {
+    console.log(err)
+    createFile("resetPin",html);
+  });
+
+});
+
+
+function createFile(name,content){
+  fs.writeFile("./"+name+".html", content, function(err) {
+    if(err) {
+      return console.log(err);
+    }
+
+    console.log("The file was saved!");
+  });
+}
